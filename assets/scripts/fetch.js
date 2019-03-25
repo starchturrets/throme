3e5def8a575e8b80178729ac557b1f2647472135
James Thaumiel <starchturrets@protonmail.com>
2019-03-25 20:15:56 +0400

    Update fetch.js

--- assets/scripts/fetch.js
+++ assets/scripts/fetch.js
@@ -44,6 +44,7 @@ function permalink(json, post_count) {
     let title = document.createElement('a');
     let permalink = json.data.children[post_count].data.permalink;
     title.setAttribute('href', 'https:i.reddit.com' + permalink);
+    title.setAttribute('target', '_blank');
     title.textContent = json.data.children[post_count].data.title;
     return title;
 }
