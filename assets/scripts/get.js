ce1e35f0cd20f4482bf052b1b296b7d8b4091d3a
James Thaumiel <starchturrets@protonmail.com>
2019-03-30 13:13:50 +0400

    Updates
    - Swapped the if/else statements for ternary operators, and ES6 template literals (fun!)
    
    -updated some styling
    
    - added a new info <span>

--- assets/scripts/get.js
+++ assets/scripts/get.js
@@ -7,27 +7,39 @@ let postCount;
 let after;
 const feed = document.querySelector('#feed');
 const renderFeed = (json) => {
+    console.log(json.data.children[0]);
     for (let count = 0; count < json.data.children.length; count++) {
+        let score = json.data.children[count].data.score;
+        let age = convertDate(json.data.children[count].data.created);
+        let author = json.data.children[count].data.author;
+        let commentNumber = json.data.children[count].data.num_comments;
+        let subreddit = json.data.children[count].data.subreddit;
+        score = eval(score / 1000).toFixed(1);
+        let url = json.data.children[count].data.url;
         let thumbNail = json.data.children[count].data.thumbnail;
-        let postMarkup;
-        if (thumbNail !== 'self' && thumbNail !== 'image' && thumbNail !== 'default' && thumbNail !== 'spoiler' && thumbNail !== 'nsfw') { //post with thumbnail
-            postMarkup = `
+        let permalink = `https://i.reddit.com${json.data.children[count].data.permalink}`;
+        let title = json.data.children[count].data.title;
+        let displayThumbNail = checkThumbNail(thumbNail);
+        let postMarkup = `
         <div class="post">
-        <a target = "_blank" href = "https://i.reddit.com${json.data.children[count].data.permalink}">${json.data.children[count].data.title}</a> 
-        <a href=${json.data.children[count].data.url} target="blank"><img alt="thumbnail" class="thumbnail" src="${json.data.children[count].data.thumbnail}"</a>
+        <a target = "_blank" href = "${permalink}">${title}</a> 
+        ${displayThumbNail ? `<a href=${url} target="blank"><img alt="thumbnail" class="thumbnail" src="${thumbNail}"</a>`: '<!--Nothing to see here, move along folks.-->'}
+        </a>
+        <span class="info"> ${score}k points submitted ${age} hours ago by <a href="https://i.reddit.com/u/${author}">${author}</a> to <a href="https://i.reddit.com/r/${subreddit}">/r/${subreddit}</a></span>
+        <a target="_blank" class="comments" href="${permalink}">${commentNumber}</a>
         </div>
         `
-        } else {
-            postMarkup = `
-        <div class="post">
-        <a target = "_blank" href = "https://i.reddit.com${json.data.children[count].data.permalink}"> ${json.data.children[count].data.title}</a> 
-        </div>`
-        }
         feed.innerHTML += postMarkup;
     }
     after = json.data.after;
-    console.log(after);
     addButton();
+}
+const checkThumbNail = (thumbNail) => {
+    if (thumbNail !== 'self' && thumbNail !== 'image' && thumbNail !== 'default' && thumbNail !== 'spoiler' && thumbNail !== 'nsfw') {
+        return true;
+    } else {
+        return false;
+    }
 }
 let count = 0;
 const addButton = () => { //Arrow functions are fun!
@@ -44,10 +56,20 @@ const addButton = () => { //Arrow functions are fun!
 const loadMore = () => {
     pageCount++;
     count += 25;
-    let url = `https://api.reddit.com/r/all?count=+${count}'&after=${after}`
+    let url = `
+            https://api.reddit.com/r/all?count=+${count}'&after=${after}`
     console.log(url);
     fetch(url)
         .then((resp) => resp.json()) // Jesus Christ, it's Json Bourne!
         .then(data => renderFeed(data));
 }
+const convertDate = (created) => {
+    //console.log(timestamp);
+    let current = Math.floor(Date.now() / 1000);
+    created = eval(current - created);
+    created = eval(created / 3600);
+    created = Math.floor(created);
+    return created;
+}
+convertDate();
 //Plans: first, use the fetch API w/ arrow functions. Then, use a combination of ES6 Template Literals, += .innerHTML, and some CSS, to add new feed items. Add a button at the bottom to load more feed items. On click, change it to a pagecount indicator and add another button on the bottom. Figure out routing and SPA functionality later.
